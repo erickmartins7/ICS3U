0aa4d2d2aa4a5bea182a89e10bbf0cb3aaea7500
erickmartins7 <159183580+erickmartins7@users.noreply.github.com>
2024-03-19 14:28:59 -0400

    Update output 1.1

---
+++ 1.1-Output.js
@@ -0,0 +1,3 @@
+console.log('Apple Inc.\nOne Apple Park Way\nCupertino, CA\n95014\n+1 408-996-1010');
+console.log('\n\n');
+console.log('Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080');
