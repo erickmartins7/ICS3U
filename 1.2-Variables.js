4f6512ebaffbb6c1303b0fc3d74f4a6385a2f751
erickmartins7 <159183580+erickmartins7@users.noreply.github.com>
2024-03-26 13:28:28 -0400

    final

---
+++ 1.2-Variables.js
@@ -0,0 +1,10 @@
+let length = 10
+let width = 5
+area = length * width
+console.log(area);
+console.log("Area:", area)
+
+let first_name = "Santa"
+let last_name = "Claws"
+last_name = "Claus"
+console.log("I still believe in", first_name, last_name);
