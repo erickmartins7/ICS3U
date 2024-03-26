fdabe97342edbb3446d5d43661d00f34d3a0be8c
erickmartins7 <159183580+erickmartins7@users.noreply.github.com>
2024-03-26 13:45:03 -0400

    final

---
+++ 1.3-Mathematics.js
@@ -0,0 +1,15 @@
+let celsius = 100
+let fahrenheit = 0
+celsius = celsius + 2
+fahrenheit = (celsius * 9/5) + 32
+console.log(fahrenheit);
+
+fahrenheit = 77
+celsius = (fahrenheit - 32) * (5/9)
+console.log(celsius);
+
+console.log ("77 degrees Fahrenheit is", celsius, "degrees celsius.");
+
+let m = 2
+m = (0 - 5)/(6 - -4)
+console.log(m);
