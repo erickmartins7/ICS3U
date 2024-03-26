e29ef674b5d4c726ec780fb2a69f6998cedfedf0
erickmartins7 <159183580+erickmartins7@users.noreply.github.com>
2024-03-26 13:16:48 -0400

    final

--- 1.1-Output.js
+++ 1.1-Output.js
@@ -1,3 +1,5 @@
 console.log('Apple Inc.\nOne Apple Park Way\nCupertino, CA\n95014\n+1 408-996-1010');
 console.log('\n\n');
 console.log('Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080');
+console.log('\n\n');
+console.log('x \to \tx \no \tx \to \nx \to \tx')
