f091664be1047622a631b281fbafeb70c98c3369
erickmartins7 <159183580+erickmartins7@users.noreply.github.com>
2024-04-02 13:55:08 -0400

    Final2

--- 1.4-UserInput.js
+++ 1.4-UserInput.js
@@ -7,3 +7,7 @@ let age = 2024 - year;
 console.log("You are likely", + age + " years old" );
 let name = prompt("What is your name? ");
 console.log("Hi, " + name + "."," You were born in", + year + " and you are likely", + age + " years old.");
+let x = name.length
+console.log("Your name is " + x + " letters long.");
+let temp1 = prompt("Enter a temperature in fahrenheit. ");
+console.log("Your temperature in celsius is " + ((temp1 - 32)*(5/9)))
