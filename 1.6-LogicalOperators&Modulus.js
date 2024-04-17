f2c96a529ec4e4ca93c77113efc4cb6cb3168b93
Erick Martins <erick.martins@stu.ocsb.ca>
2024-04-17 18:13:46 -0400

    finished

--- 1.6-LogicalOperators&Modulus.js
+++ 1.6-LogicalOperators&Modulus.js
@@ -2,14 +2,24 @@ const prompt = require('prompt-sync')();
 
 let user_input = prompt("Do you want to play a game? (y/n): ");
 
-if (user_input == y){
-    let num1 = prompt("Enter a number: ");
+if ((user_input == "y") || (user_input == "")){
+    console.log("Time to play.")
 }
+else{
+    console.log("Please make a proper selection")
+}
+
+let num1 = prompt("Enter a number: ");
+num1 = Number(num1)
 if (num1 > 10 && num1 < 50){
+    console.log("Your next number should be greater than 50.")
+}
+else{
     console.log("Your next number should be greater than 50.")
 }
 
 let num2 = prompt("Enter a number: ");
+num2 = Number(num2)
 if (num2 % 2 == 0){
     console.log("Your next number should be odd.")
 }
@@ -18,3 +28,18 @@ else if (num2 % 2 != 0){
 }
 
 let num3 = prompt("Enter a number: ");
+num3 = Number(num3)
+
+let sum = num1 + num2 + num3
+if ((sum + 1) % 10 == 0){
+    console.log("Your numbers are Squirrelly.")
+}
+else {
+    console.log("Your numbers are not Squirrelly.")
+}
+if ((num1 % 3 == 0) && (num2 % 3 == 0) || (num1 % 3 == 0) && (num3 % 3 == 0) || (num2 % 3 == 0) && (num3 % 3 == 0)){
+    console.log("Your numbers are Jazzy.")
+}
+else {
+    console.log("Your numbers are not Jazzy.")
+}
